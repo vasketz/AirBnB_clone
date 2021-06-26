e5dfd3f9a54d5979265d172bc9a9dff2161447f4
vasketz <anfevach@gmail.com>
2021-06-26 01:27:41 +0000

    update first part of task 3

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,24 @@
 #!/usr/bin/python3
 # Defines all common attributes/methods for other classes
+from uuid import uuid4
+from datetime import datetime
+
 
 class BaseModel():
     """
     parent class BaseModel
     """
+    id = str(uuid4())
+    create_at = datetime.now()
+    update_at = datetime.now()
+
+    def __str__(self):
+        """Return string"""
+        return str("[BaseModel] ({}) {}".format(self.id, self.__dict__))
+
+    def to_dict(self):
+        new_dic = {'update_at':self.update_at, 'create_at':self.create_at, '__class__':self.__class__.__name__}
+        return dict(self.__dict__, **new_dic)
+
+    def save(self):
+        self.update_at = datetime.now()
---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Test base
+"""
+from models.base_model import BaseModel
+
+
+my_model = BaseModel()
+my_model.name = "Holberton"
+my_model.my_number = 89
+print(my_model)
+my_model.save()
+print(my_model)
+my_model_json = my_model.to_dict()
+print(my_model_json)
+print("JSON of my_model:")
