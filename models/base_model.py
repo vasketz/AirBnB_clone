baf0fea44d97fa2fc34cac4ab9ada304df40349d
mareizaa <marce.3210@gmail.com>
2021-06-26 12:22:36 -0500

    Constructor

--- models/base_model.py
+++ models/base_model.py
@@ -1,17 +1,28 @@
 #!/usr/bin/python3
-# Defines all common attributes/methods for other classes
-from uuid import uuid4
+"""Defines all common attributes/methods for other classes"""
+import uuid
 from datetime import datetime
-
 
 class BaseModel():
     """
     parent class BaseModel
     """
 
-    id = str(uuid4())
-    create_at = datetime.now()
-    update_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        """
+        Contructor
+        """
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "update_at":
+                    setattr(self, key, datetime.strptime(str(value), "%Y-%m-%dT%H:%M:%S.%f"))
+                else:
+                    setattr(self, key, value)
+
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.update_at = datetime.now()
 
     def __str__(self):
         """Return string"""
@@ -19,7 +30,7 @@ class BaseModel():
 
     def to_dict(self):
         new_dic = {'update_at': self.update_at.isoformat(),
-                   'create_at': self.create_at.isoformat(),
+                   'create_at': self.created_at.isoformat(),
                    '__class__': self.__class__.__name__}
         return dict(self.__dict__, **new_dic)
 
@@ -29,7 +40,6 @@ class BaseModel():
     #   new_dict.update(__class__=name, creat_at=self.create_at.isoformat())
     #else:
     #   
-
 
     def save(self):
         self.update_at = datetime.now()
