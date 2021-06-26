ed1b20db0f0ea9b7a0e1551fbfb0520d684835e1
vasketz <anfevach@gmail.com>
2021-06-26 16:31:43 +0000

    Update task 3

--- models/base_model.py
+++ models/base_model.py
@@ -8,6 +8,7 @@ class BaseModel():
     """
     parent class BaseModel
     """
+
     id = str(uuid4())
     create_at = datetime.now()
     update_at = datetime.now()
@@ -17,8 +18,19 @@ class BaseModel():
         return str("[BaseModel] ({}) {}".format(self.id, self.__dict__))
 
     def to_dict(self):
-        new_dic = {'update_at':self.update_at, 'create_at':self.create_at, '__class__':self.__class__.__name__}
+        new_dic = {'update_at': self.update_at.isoformat(),
+                   'create_at': self.create_at.isoformat(),
+                   '__class__': self.__class__.__name__}
         return dict(self.__dict__, **new_dic)
+
+    #name = self.__class__.__name__
+    #new_dict = self.__dict__.copy()
+    #if new_dict[create_at]:
+    #   new_dict.update(__class__=name, creat_at=self.create_at.isoformat())
+    #else:
+    #   
+
 
     def save(self):
         self.update_at = datetime.now()
+
