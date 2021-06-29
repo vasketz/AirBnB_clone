2cc3bdef590133658e05c37e95a038b04baef193
vasketz <anfevach@gmail.com>
2021-06-29 02:20:40 +0000

    Update console.py

--- console.py
+++ console.py
@@ -3,8 +3,10 @@
 program to console.py
 """
 import cmd
+from models import base_model
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
@@ -29,7 +31,7 @@ class HBNBCommand(cmd.Cmd):
         """
         lista = args.split(" ")
 
-        if lista[0] == "BaseModel" :
+        if lista[0] == "BaseModel":
             obj = BaseModel()
             print("{}".format(obj.id))
         elif lista[0] != "BaseModel":
@@ -42,8 +44,23 @@ class HBNBCommand(cmd.Cmd):
         based on the class name and id
         """
         lista = args.split(" ")
-        res = BaseModel.__str__.__class__.__name__
-        print(res)
+        res = storage.all()
+        if lista[0] is "":
+            print("** class name missing **")
+        elif lista[0] != "BaseModel":
+            print("** class doesn't exist **")
+        elif len(lista) == 1:
+            print("** instance id missing **")
+        else:
+            key = "BaseModel.{}".format(lista[1])
+            if key is not res:
+                print("** no instance found **")
+            else:
+                for keys, value in res.items():
+                    if keys == key:
+                        print(keys)
+           
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
