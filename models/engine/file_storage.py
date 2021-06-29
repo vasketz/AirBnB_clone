b30c87408bf6891f7c307545c8a5e9ab00dc9f79
vasketz <anfevach@gmail.com>
2021-06-29 20:54:09 +0000

    Update pep8

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -38,8 +38,10 @@ class FileStorage():
 
     def reload(self):
         """
-        deserializes the JSON file to __objects (only if the JSON file(__file_path)
-        exists: otherwise, do nothing. if the file doesn't exist, no exception should be raised)
+        deserializes the JSON file to __objects
+        (only if the JSON file(__file_path)
+        exists: otherwise, do nothing. if the file doesn't exist,
+        no exception should be raised)
         """
         try:
             with open(self.__file_path, "r", encoding="utf-8") as file:
