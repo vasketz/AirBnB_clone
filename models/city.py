3423624d8f42f8dc1abe450ec4337cc095edb043
mareizaa <marce.3210@gmail.com>
2021-06-30 22:17:44 -0500

    update class city

---
+++ models/city.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""class city that inherits from BaseModel"""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """class city that inherits from BaseModel"""
+    name = ""
+    state_id = ""
